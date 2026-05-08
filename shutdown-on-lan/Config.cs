99955d7faa08c1b4949bce3e677528fce02c4754
semandrr <236989810+semandrr@users.noreply.github.com>
2026-05-08 21:12:59 +0700

    unneeded hyphens + forgot to replace WOL with WoL

--- shutdown-on-lan/Config.cs
+++ shutdown-on-lan/Config.cs
@@ -3,7 +3,7 @@ namespace shutdown_on_lan;
 public class Config
 {
     public static readonly int wolPort = 9; // usually 9 or 7
-    public static readonly string macAddr = "edit_me"; // only WOL-packets for that MAC-address will trigger shutdown, all others will be ignored
+    public static readonly string macAddr = "edit_me"; // only WoL packets for that MAC-address will trigger shutdown, all others will be ignored
     public static readonly int instantShutdown = 1; // trigger instant shutdown, set this to 0 if you want to avoid possible accidents
-    public static readonly int silent = 0; // hide messages about ignoring WoL-packets sent to different MAC-address
+    public static readonly int silent = 0; // hide messages about ignoring WoL packets sent to different MAC-address
 }
